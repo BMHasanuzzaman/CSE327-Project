92dd2dfd413323601a5db37737fc5783954681fd
BMHasanuzzaman <47479370+BMHasanuzzaman@users.noreply.github.com>
2019-04-10 21:01:26 +0600

    Update index.php
    log in page is coonected with database. and works

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+$connect = mysqli_connect("localhost", "root", "", "project327");
+?>
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -15,32 +22,61 @@
     <div class="modal-dialog modal-dialog-centered text-center">
         <div class="col-sm-8 modal-container">
             <div class="modal-content">
-               
+
                 <div class="col-12 user-img">
                     <img src="image/avatar/smoke.png" width="100" height="100">
                 </div>
-                
+
                 <div class="modal-body">
-                    <form>
+                    <form method="POST">
                         <div class="form-group">
                             <label for="email">Email address</label>
-                            <input type="email" class="form-control" id="email"  placeholder="Enter email">
-                            
+                            <input type="email" class="form-control" id="email"  placeholder="Enter Email" name="email">
+
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password" placeholder="Password">
+                            <input type="password" class="form-control" id="password" placeholder="Password" name="pass">
                             <!-- For js genarated warning for wrong password -->
                             <small>Warning</small>
-                            
+
                         </div>
-                        <button type="submit" class="btn btn-outline-danger">Login</button>
+                        <input type="submit" class="btn btn-outline-danger" name="submit" value="login">
                         </div class="form-group">
                             <a href="#">Forgot password?</a>
                             <a href="signup.php">Create account</a>
                         </div>
                     </form>
                 </div>
+
+
+		    <?php
+            if (isset($_POST['submit']))
+			{
+                if (isset($_POST["email"]))
+				{
+                    $email = $_POST['email'];
+                    $pass = $_POST['pass'];
+                    $query = "select * from user WHERE email='$email' AND pass='$pass'";
+                    $result = mysqli_query($connect, $query);
+                    $num = mysqli_num_rows($result);
+                    if ($num > 0)
+					{
+                        $row = mysqli_fetch_array($result);
+
+                        header('location:profile.php');
+                    }  // login with the student id ends here
+
+						else
+						{ // this 'else' using for both students and faculties invalid login
+                            echo '<script type="text/javascript"> alert("Usrname and Password did not match ") </script>';
+                        }
+
+                }
+			}
+            ?>
+
+
             </div>
         </div>
     </div>
